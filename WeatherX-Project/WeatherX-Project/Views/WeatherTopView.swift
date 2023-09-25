2c6ab06da081ffda0ac9bb0e4c3cd6d75c5c0250
Soobean <saen5848@naver.com>
2023-09-25 21:48:01 +0900

    [Add] : WeatherTopView file 생성, 1번 topView UI 작성

---
+++ WeatherX-Project/WeatherX-Project/Views/WeatherTopView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherTopView.swift
+//  WeatherX-Project
+//
+//  Created by 허수빈 on 2023/09/25.
+//
+
+import UIKit
+
+class WeatherTopView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
