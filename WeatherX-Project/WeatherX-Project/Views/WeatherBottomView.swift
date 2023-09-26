9eab2898fdc658fc22b4175bbd126d491aaba8f9
kwangjo93 <125628009+kwangjo93@users.noreply.github.com>
2023-09-26 14:09:26 +0900

    [Feat]: Mylocation UI 구현

---
+++ WeatherX-Project/WeatherX-Project/Views/WeatherBottomView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherBottomView.swift
+//  WeatherX-Project
+//
+//  Created by 천광조 on 2023/09/26.
+//
+
+import UIKit
+
+class WeatherBottomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
