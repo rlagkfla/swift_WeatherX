4bb84ce17ca503875b99b2c54365d8c0b76b0782
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2023-09-26 20:55:41 +0900

    [FEAT] 메뉴 추가 (기온, 바람), Back 버튼 "완료" 버튼으로 변경

--- WeatherX-Project/WeatherX-Project/Controllers/MapViewController.swift
+++ WeatherX-Project/WeatherX-Project/Controllers/MapViewController.swift
@@ -15,14 +15,28 @@ class MapViewController: UIViewController {
     private var coord: Coord?
     private var main: Main?
     private var name: String?
+    private var selectedAction: String = "기온" // 기본값
     
     var weatherResponse: WeatherResponse?
-    var networking = Networking.shared
+    
+    private lazy var menu: UIMenu = {
+        let tempAction = UIAction(title: "기온", image: UIImage(systemName: "thermometer.medium"), handler: { [weak self] _ in
+            self?.handleTemperature()
+        })
+
+        let windAction = UIAction(title: "바람", image: UIImage(systemName: "wind"), handler: { [weak self] _ in
+            self?.handleWind()
+        })
+
+        let menu = UIMenu(title: "", children: [tempAction, windAction])
+        return menu
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupMapView()
+        configureNav()
         updateUserLocation()
         
     }
@@ -49,6 +63,32 @@ private extension MapViewController {
             print("coord가 nil입니다.")
         }
     }
+    
+    func configureNav() {
+        let doneButton = UIBarButtonItem(title: "완료",
+                                         style: .plain,
+                                         target: self,
+                                         action: #selector(doneButtonTapped))
+        doneButton.tintColor = .black
+        navigationItem.leftBarButtonItem = doneButton
+        
+        let rightBarButton = UIBarButtonItem(image: UIImage(systemName: "square.3.stack.3d"), style: .plain, target: nil, action: nil)
+        rightBarButton.menu = menu
+        rightBarButton.tintColor = .black
+        navigationItem.rightBarButtonItem = rightBarButton
+    }
+    
+    @objc func doneButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func handleTemperature() {
+        selectedAction = "기온"
+    }
+    
+    func handleWind() {
+        selectedAction = "바람"
+    }
 }
 
 extension MapViewController: MKMapViewDelegate {
@@ -58,9 +98,26 @@ extension MapViewController: MKMapViewDelegate {
         }
         
         let annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "CustomMarker")
-        if let temp = self.weatherResponse?.main.temp {
-            annotationView.glyphText = "\(temp)°"
+        
+        switch selectedAction {
+        case "기온":
+            if let temp = self.weatherResponse?.main.temp {
+                annotationView.glyphText = "\(temp)°"
+            } else {
+                print("temp is nil")
+            }
+        case "바람":
+            if let windSpeed = self.weatherResponse?.wind.speed {
+                annotationView.glyphText = "\(windSpeed) m/s"
+            } else {
+                print("windspeed is nil")
+            }
+        default:
+            break
         }
+//        if let temp = self.weatherResponse?.main.temp {
+//            annotationView.glyphText = "\(temp)°"
+//        }
         
         return annotationView
     }
