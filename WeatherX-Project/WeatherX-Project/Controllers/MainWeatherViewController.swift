06f1c465f4a1ccb30319782f33eac7e369a738a0
kwangjo93 <125628009+kwangjo93@users.noreply.github.com>
2023-09-26 23:12:53 +0900

    [Add] : MainWeatherViewController.swift 파일 추가

---
+++ WeatherX-Project/WeatherX-Project/Controllers/MainWeatherViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainWeatherViewController.swift
+//  WeatherX-Project
+//
+//  Created by 천광조 on 2023/09/26.
+//
+
+import UIKit
+
+class MainWeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
