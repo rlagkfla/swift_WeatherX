3553404c14f32bcabe69ef4ab79689896297be0f
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2023-10-01 12:36:28 +0900

    [Add] 소수점 반올림 Extension추가, [Design] 미들뷰 UI 수정

--- WeatherX-Project/WeatherX-Project/Views/Cell/WeatherCollectionViewCell.swift
+++ WeatherX-Project/WeatherX-Project/Views/Cell/WeatherCollectionViewCell.swift
@@ -18,9 +18,9 @@ class WeatherCollectionViewCell: UICollectionViewCell {
         lb.textColor = UIColor(red: 0.179, green: 0.179, blue: 0.179, alpha: 1)
         lb.font = UIFont.systemFont(ofSize: 26, weight: .bold)
         
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.lineHeightMultiple = 0.73
-        lb.attributedText = NSMutableAttributedString(string: "21º", attributes: [NSAttributedString.Key.paragraphStyle: paragraphStyle])
+//        let paragraphStyle = NSMutableParagraphStyle()
+//        paragraphStyle.lineHeightMultiple = 0.73
+//        lb.attributedText = NSMutableAttributedString(string: "21º", attributes: [NSAttributedString.Key.paragraphStyle: paragraphStyle])
         
         return lb
     }()
@@ -36,9 +36,9 @@ class WeatherCollectionViewCell: UICollectionViewCell {
         let lb = UILabel()
         lb.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
         lb.font = UIFont.systemFont(ofSize: 18)
-        let timeParagraphStyle = NSMutableParagraphStyle()
-        timeParagraphStyle.lineHeightMultiple = 1.14
-        lb.attributedText = NSMutableAttributedString(string: "9 AM", attributes: [NSAttributedString.Key.paragraphStyle: timeParagraphStyle])
+//        let timeParagraphStyle = NSMutableParagraphStyle()
+//        timeParagraphStyle.lineHeightMultiple = 1.14
+//        lb.attributedText = NSMutableAttributedString(string: "9 AM", attributes: [NSAttributedString.Key.paragraphStyle: timeParagraphStyle])
         
         return lb
     }()
@@ -48,8 +48,8 @@ class WeatherCollectionViewCell: UICollectionViewCell {
         let sv = UIStackView(arrangedSubviews: [timetemper,imageView,timeLabel])
         sv.axis = .vertical
         sv.distribution = .fill
-        sv.alignment = .fill
-        sv.spacing = 10
+//        sv.alignment = .fill
+        sv.spacing = 5
         sv.alignment = .center
         return sv
     }()
@@ -70,27 +70,27 @@ class WeatherCollectionViewCell: UICollectionViewCell {
     private func cellLayout(){
         self.addSubview(stackView)
 
-        timetemper.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.top.equalToSuperview().offset(3)
-        }
+//        timetemper.snp.makeConstraints {
+//            $0.centerX.equalToSuperview()
+//            $0.top.equalToSuperview().offset(3)
+//        }
         imageView.snp.makeConstraints {
-            $0.width.equalTo(40)
-            $0.height.equalTo(32)
-            $0.leading.equalToSuperview().offset(4)
-            $0.top.equalTo(timetemper.snp.bottom).offset(4)
+//            $0.width.equalTo(40)
+            $0.height.equalTo(48)
+//            $0.leading.equalToSuperview().offset(4)
+//            $0.top.equalTo(timetemper.snp.bottom).offset(4)
         }
-        timeLabel.snp.makeConstraints {
-            $0.width.equalTo(44)
-            $0.height.equalTo(22)
-            $0.centerX.equalTo(imageView)
-            $0.top.equalTo(imageView.snp.bottom).offset(12)
-        }
+//        timeLabel.snp.makeConstraints {
+//            $0.width.equalTo(44)
+//            $0.height.equalTo(22)
+//            $0.centerX.equalTo(imageView)
+//            $0.top.equalTo(imageView.snp.bottom).offset(12)
+//        }
         
         stackView.snp.makeConstraints {
             $0.width.equalToSuperview()
             $0.height.equalToSuperview()
-            $0.edges.equalToSuperview()
+//            $0.edges.equalToSuperview()
         }
     }
    
